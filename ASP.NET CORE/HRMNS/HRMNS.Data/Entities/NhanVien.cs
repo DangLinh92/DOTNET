fa7452a60fdf4faac15a6620c35f26fc522ab75e
DangLinh92 <hoamoclan.3192@gmail.com>
2021-12-27 22:10:27 +0700

    update info

--- ASP.NET CORE/HRMNS/HRMNS.Data/Entities/NhanVien.cs
+++ ASP.NET CORE/HRMNS/HRMNS.Data/Entities/NhanVien.cs
@@ -13,5 +13,52 @@ namespace HRMNS.Data.Entities
         public Status Status { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
+
+        /// <summary>
+        /// Chức danh
+        /// </summary>
+        public string TeamPosition { get; set; }
+
+        /// <summary>
+        /// Bộ phận
+        /// </summary>
+        public string Dept { get; set; }
+
+        /// <summary>
+        /// Họ tên
+        /// </summary>
+        public string Name { get; set; }
+        // Giới tính
+        public string Sex { get; set; }
+        //Ngày sinh
+        public string Age { get; set; }
+        // Nơi sinh
+        public string At { get; set; }
+        // Tình trạng hôn nhân
+        public string MarriedStatus { get; set; } // married or single
+        // Dân tộc
+        public string Race { get; set; }
+        // Địa chỉ thường trú
+        public string HomeAdd{get;set;}
+        public string PhoneNumber { get; set; }
+        // Người thân
+        public virtual ICollection<Relative> Relatives { get; set; }
+        public string CMTND { get; set; }
+        // Ngay cap
+        public string IssuedDate { get; set; }
+        // Noi cap
+        public string IssuedPlace { get; set; }
+        // So TK
+        public string AccountNo { get; set; }
+        // Trường đào tạo
+        public string Education { get; set; }
+        public string UserCreated { get; set; }
+        public string UserModified { get; set; }
+        // Cấp bậc
+        public virtual ICollection<GradeNhanVien> GradeNhanViens { get; set; }
+        // Ngày vào
+        public string StartingWorking { get; set; }
+        // Ngày kết thúc thử việc
+        public string EndDateTRContract { get; set; }
     }
 }
--- ASP.NET CORE/HRMNS/HRMNS.Data/Interfaces/IDateTracking.cs
+++ ASP.NET CORE/HRMNS/HRMNS.Data/Interfaces/IDateTracking.cs
@@ -8,5 +8,7 @@ namespace HRMNS.Data.Interfaces
     {
         DateTime DateCreated { get; set; }
         DateTime DateModified { get; set; }
+        string UserCreated { get; set; }
+        string UserModified { get; set; }
     }
 }
