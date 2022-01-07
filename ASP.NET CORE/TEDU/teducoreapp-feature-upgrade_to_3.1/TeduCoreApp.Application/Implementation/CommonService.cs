18411273fbe36be7a6c6908c9477ec0f3592b04f
danglv <danglevan.9919@gmail.com>
2022-01-07 13:46:30 +0700

    add Tedu 3.1

---
+++ ASP.NET CORE/TEDU/teducoreapp-feature-upgrade_to_3.1/TeduCoreApp.Application/Implementation/CommonService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeduCoreApp.Application.Interfaces;
+using TeduCoreApp.Application.ViewModels.Blog;
+using TeduCoreApp.Application.ViewModels.Common;
+using TeduCoreApp.Data.Entities;
+using TeduCoreApp.Infrastructure.Interfaces;
+using TeduCoreApp.Utilities.Constants;
+using TeduCoreApp.Utilities.Dtos;
+using TeduCoreApp.Utilities.Helpers;
+
+namespace TeduCoreApp.Application.Implementation
+{
+    public class CommonService : ICommonService
+    {
+        private IRepository<Footer, string> _footerRepository;
+        private IRepository<SystemConfig, string> _systemConfigRepository;
+        private IUnitOfWork _unitOfWork;
+        private IRepository<Slide, int> _slideRepository;
+        private readonly IMapper _mapper;
+
+        public CommonService(IRepository<Footer, string> footerRepository,
+            IRepository<SystemConfig, string> systemConfigRepository,
+            IUnitOfWork unitOfWork, IMapper mapper,
+            IRepository<Slide, int> slideRepository)
+        {
+            _footerRepository = footerRepository;
+            _unitOfWork = unitOfWork;
+            _systemConfigRepository = systemConfigRepository;
+            _slideRepository = slideRepository;
+            _mapper = mapper;
+        }
+
+        public FooterViewModel GetFooter()
+        {
+            return _mapper.Map<Footer, FooterViewModel>(_footerRepository.FindSingle(x => x.Id ==
+            CommonConstants.DefaultFooterId));
+        }
+
+        public List<SlideViewModel> GetSlides(string groupAlias)
+        {
+            return _mapper.ProjectTo<SlideViewModel>(
+                _slideRepository.FindAll(x => x.Status && x.GroupAlias == groupAlias))
+                .ToList();
+        }
+
+        public SystemConfigViewModel GetSystemConfig(string code)
+        {
+            return _mapper.Map<SystemConfig, SystemConfigViewModel>(_systemConfigRepository.FindSingle(x => x.Id == code));
+        }
+    }
+}
